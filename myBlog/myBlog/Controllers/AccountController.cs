b35189e09555ee068b5ddd0fbbb24429e11f51ca
a505606339 <chushangyiban@126.com>
2016-08-25 19:17:11 +0800

    添加了实体操作类

---
+++ myBlog/myBlog/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.LoginState = "登录前";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            string email = fc["inputEmail3"];
+            string name = fc["inputPassword3"];
+            
+            ViewBag.LoginState = email + "登录后";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ myBlog/myBlog/Models/SysRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myBlog.Models
+{
+    public class SysRole
+    {
+        public int ID { get; set; }
+
+        public string RoleName { get; set; }
+
+        public string RoleDesc { get; set; }
+
+        public virtual ICollection<SysUserRole> SysUserRoles { get; set; }
+    }
+}
